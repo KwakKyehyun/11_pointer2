2df5d0775c0924517d2092b32cf578503a988ef2
KwakKyehyun <55428343+KwakKyehyun@users.noreply.github.com>
2019-11-19 22:00:59 +0900

    submission03_update

--- main.c
+++ main.c
@@ -1,19 +1,43 @@
 #include <stdio.h>
 
 
+
+
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-    char*proverb="All that glisters is not gold.";
-    void setPointer(char**q)
-    {
-    	q = proverb;
-	}
-	int main(void)
-	{
-		char**p = "zzz";
-		setPointer(&p);
-		printf("%s\n",p);
-		
-		return 0;
-	}
+
+
+int main(int argc, char *argv[]) {
+
 	
+
+	char buffer[8];
+
+	double *pd;
+
+	int *pi;
+
+	
+
+	pd = (double *)buffer;
+
+	*pd = 3.14;
+
+	printf("%f\n", *(double*)buffer);
+
+	
+
+	pi = (int *)buffer;
+
+	*pi = 123;
+
+	*(pi+1) = 456;
+
+	printf("%d %d\n", *(int*)buffer, *((int*)buffer + 1));
+
+	
+
+	return 0;
+
+}
